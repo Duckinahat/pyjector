a277f0f4fd27ac8599cd5650ca7f3d58da2ba2cc
Nicola Leonardi <nicolaleonardi@gmail.com>
2016-06-09 15:46:12 -0700

    Not working, will try on newer version of gstreamer

--- pyjector/trickplayer.py
+++ pyjector/trickplayer.py
@@ -3,12 +3,15 @@
 import sys
 import os
 import logging
+import threading
 
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import GObject, Gst
+from gi.repository import GObject, Gst, GstVideo
 
-from rtpsink import rtpsink
+
+logger = logging.getLogger(__name__)
+
 
 class TrickPlayer():
 
@@ -18,11 +21,18 @@ class TrickPlayer():
         self.video_sink = None
         self.playing = False
         self.rate = 1.0
+        self.loop = True
 
     def set_pipeline(self):
         self.pipeline = Gst.parse_launch("playbin uri=file://%s" % self.file)
-        videosink = Gst.ElementFactory.make("autovideosink")
-        self.pipeline.set_property('video_sink', videosink)
+        self.set_video_sink()
+        self.pipeline.set_property('video_sink', self.video_sink)
+
+    def set_video_sink(self):
+        pass
+
+    def run(self):
+        pass
 
     def __send_seek_event(self):
         fmat = Gst.Format.TIME
@@ -55,13 +65,39 @@ class TrickPlayer():
 
         self.video_sink.send_event(seek_event)
 
-        print("Current rate: %d" % self.rate)
+        logger.info("Current rate: %d", self.rate)
+
+    def update_color_channel(self, channel_name, step_size):
+        color_balance = self.pipeline.list_channels()
+        channels = color_balance.list_channels()
+        for ch in channels:
+            if (ch.label == channel_name):
+                channel = ch
+        if (not channel):
+            return
+
+        step = step_size * (channel.max_value - channel.min_value)
+        value = color_balance.get_value(channel)
+        value += step
+        if (value > channel.max_value):
+            value = channel.max_value
+        elif (value < channel.min_value):
+            value = channel.min_value
+        color_balance.set_value(channel, value)
+
+    def __start(self):
+        self.pipeline.set_state(Gst.State.PLAYING)
+        self.playing = True
+
+    def __stop(self):
+        self.pipeline.set_state(Gst.State.NULL)
+        self.playing = False
 
     def change_file(self, newfile):
         self.file = newfile
-        self.pipeline.set_state(Gst.State.NULL)
+        self.__stop()
         self.pipeline.set_property('uri', "file://%s" % newfile)
-        self.pipeline.set_state(Gst.State.PLAYING if self.playing else Gst.State.PAUSED)
+        self.__start()
 
     def cleanup(self):
         self.pipeline.set_state(Gst.State.NULL)
@@ -69,9 +105,9 @@ class TrickPlayer():
             self.video_sink.unref()
         self.pipeline.unref()
 
-    def play_pause(self):
+    def pause_play(self):
+        self.pipeline.set_state(Gst.State.PAUSED if self.playing else Gst.State.PLAYING)
         self.playing = not self.playing
-        self.pipeline.set_state(Gst.State.PLAYING if self.playing else Gst.State.PAUSED)
 
     def set_speed(self, rate):
         self.rate = rate
@@ -80,6 +116,9 @@ class TrickPlayer():
     def reverse(self):
         self.rate *= -1.0
         self.__send_seek_event()
+
+    def set_marker(self):
+        pass
 
     def step_frame(self):
         if (self.video_sink == None):
